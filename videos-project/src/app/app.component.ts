1f1a6e63606056aefb46eddab2f29273c00568af
gabyortova <gabi.ortova@gmail.com>
2024-11-29 18:36:53 +0200

    added simple footer

--- videos-project/src/app/app.component.ts
+++ videos-project/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationBarComponent],
+  imports: [RouterOutlet, NavigationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'videos-project';
