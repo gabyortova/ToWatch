e57e1421155b42e33ce714de4722c2d3f0e7762b
gabyortova <gabi.ortova@gmail.com>
2024-12-08 01:21:08 +0200

    added create page

--- videos-project/src/app/app.routes.ts
+++ videos-project/src/app/app.routes.ts
@@ -1,18 +1,19 @@
+import { CreateVideoComponent } from './create-video/create-video.component';
 import { ToWatchComponent } from './to-watch/to-watch.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CatalogComponent } from './catalog/catalog.component';
-// import { Component } from '@angular/core';
+// import { Component } from '@angulto-watch/to-watch.component';/core';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
-
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
-  { path: 'towatch', component: ToWatchComponent },
+  { path: 'to-watch', component: ToWatchComponent },
+  { path: 'create-video', component: CreateVideoComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: '404', component: ErrorPageComponent },
