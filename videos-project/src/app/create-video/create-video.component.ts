88ed8a55ceb167d8dca652a26ee3998a50d9ccfc
gabyortova <gabi.ortova@gmail.com>
2024-12-08 02:33:59 +0200

    create video fixes

--- videos-project/src/app/create-video/create-video.component.ts
+++ videos-project/src/app/create-video/create-video.component.ts
@@ -1,11 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Component } from '@angular/core';
+import { ApiService } from './../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-video',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './create-video.component.html',
-  styleUrl: './create-video.component.css'
+  styleUrl: './create-video.component.css',
 })
 export class CreateVideoComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  addVideo(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const { title, videoUrl, description, img } = form.value;
+
+    this.apiService.createVideo(title, videoUrl, description, img).subscribe(() => {
+      this.router.navigateByUrl('/catalog');
+    });
+  }
 }
